7e6d0948363326cd7b1b9a1dd373065548ac5df3
onili <supayuta0925@gmail.com>
2023-03-11 16:38:13 +0900

    [FIX] tokenを環境変数から取得するように修正しました。

--- src/kaihatu.py
+++ src/kaihatu.py
@@ -1,27 +1,22 @@
 import discord
 from discord.ext import commands
 import discord.app_commands
+import os
 
-TOKEN = 'MTA4MzMzNzc0MDc4OTIyMzQ3NA.Gl_ni9.Eqasp5R_HpzELt3M_QXLC6lSdFqK1-yyLOlEGA'
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 bot = commands.Bot(command_prefix='にゃん', intents=intents)
 
-
-
-
-
 @bot.event
 async def on_ready():
     print('Botが起動しましたにゃん！')
 
-
 @bot.command()
 async def なでる(ctx):
     await ctx.send(':cat: にゃーん')
-
 
 @bot.command()
 async def たたく(ctx):
